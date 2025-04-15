e23d346a2547f185a058359542aa1053ebc661e5
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-15 13:05:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,6 @@
 terraform {
   required_version = ">= 1.10.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.87.0"
-    }
-  }
+  
 }
  
 provider "aws" {
